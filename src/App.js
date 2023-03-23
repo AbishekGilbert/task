86c8a3a00b9a067844bd69945881a88090d8732e
AbishekGilbert <abishek.gilbert0511@gmail.com>
2023-03-23 11:02:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Card1 from './Card1';
+import Card2 from './Card2';
+import Card3 from './Card3';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div classNamw="container d-flex justify-content-center gap-4 flex-wrap flex-row p-3 bg-color">
+      <Card1 />
+      <Card2 />
+      <Card3 />
+      </div>
     </div>
   );
 }
